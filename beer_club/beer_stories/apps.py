c06925a55c01a7e007550e7c9e9a3248acea90d4
Your Name <lukasjonaitisltu@gmail.com>
2024-03-27 11:41:04 +0200

    appsas

---
+++ beer_club/beer_stories/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _ 
+
+
+class BeerStoriesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'beer_stories'
+    verbose_name = ('beer_stories')
